6ba88b0a748aa93dfe1fc933b0046b6f9df70a8a
mejriIbtihel <48315717+mejriIbtihel@users.noreply.github.com>
2019-03-28 11:39:23 +0100

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,10 @@
+class Contact extends React.Component {
+  render() {
+    return (
+        <div className="content">
+           <h1>bonjour </h1>
+      </div>
+    );
+  }
+}
+export default Contact;
